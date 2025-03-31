ba63ecafdbee5af799cf8d8ad30fa201f05a7342
dghyuiok <14341407+dghyuiok@user.noreply.gitee.com>
2025-03-31 22:00:45 +0800

    给测试接口添加Slf4j，并添加了个log.info信息

--- backend/src/main/java/com/wtu/controller/TestController.java
+++ backend/src/main/java/com/wtu/controller/TestController.java
@@ -3,12 +3,14 @@ package com.wtu.controller;
 import com.wtu.result.Result;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @Tag(name = "测试接口")
+@Slf4j
 public class TestController {
 
 
@@ -16,6 +18,7 @@ public class TestController {
      @GetMapping("/test")
      @Operation(description = "测试接口")
      public Result test() {
+            log.info("测试接口正在运行");
          return Result.success("test");
      }
 
