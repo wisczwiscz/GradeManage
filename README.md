7efc85cdfc0ba1a7e66d09fa92bb43b4532a0f0b
coderxguan <3288733739@qq.com>
2025-03-31 19:26:02 +0800

    create backend

---
+++ README.md
@@ -0,0 +1,15 @@
+# 后端项目
+
+
+
+
+
+# 前端
+
+
+## 项目安装
+```shell
+npm install
+npm run dev
+```
+
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div >
+    <h1>前端页面</h1>
+    <p>这是一个前端页面</p>
+    <p>这是一个前端页面, 已完成前端项目构建</p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
